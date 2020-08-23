537b1b21ca45570b932db7e817fb5fc0dc739022
Denilson <denilsonsousa7@outlook.com>
2020-08-23 10:45:12 -0300

    logica add msgs feito, exclusão msg feito

--- js/newMessage.js
+++ js/newMessage.js
@@ -2,40 +2,74 @@ const inputNome = document.getElementById("input-name");
 const inputMessage = document.getElementById("input-message");
 const ul = document.getElementById("messages");
 const messages = [];
-
+var idElement = 0;
 
 const form = document.getElementById("form-newMessage");
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if(inputNome.value != null && inputNome.value != '' && inputMessage.value != null && inputMessage.value != ''){
-        messages.push({name: inputNome.value, message: inputMessage.value});
-        generateMessage()
+    if (inputNome.value != null && inputNome.value != '' && inputMessage.value != null && inputMessage.value != '') {
+        messages.push({idElement, name: inputNome.value, message: inputMessage.value });
+        idElement++;
+        refreshMessages();
+       
     }
 
-    
+
 })
 
-
-function generateMessage(){
-    var li = document.createElement("li");
-    let div = document.createElement("div");
-
-
-    let nameValue = document.createTextNode(inputNome.value);
-    let messageValue = document.createTextNode(inputMessage.value);
-
+function clearUl(){
+    let lis = document.getElementsByClassName("messagesInd");
+    let x = ul.childElementCount;
+    for(var i = 0; i < x; i ++){
+        ul.removeChild(lis[0]);
+    }   
     
-    let h2NameValue = document.createElement("h2");
-    h2NameValue.appendChild(nameValue);
-    let h2MessageValue = document.createElement("h2");
-    h2MessageValue.appendChild(messageValue);
+}
+
+function deleteMessage(id){
+
+    messages.forEach(element => {
+        if(element.idElement == id){
+            
+            messages.splice(messages.indexOf(element),1);
+        }
+    });
+}
+function refreshMessages() {
+    clearUl();
+    console.log(messages);
+    messages.forEach(element => {
+        let li = document.createElement("li");
+        li.id = element.idElement;
+        let div = document.createElement("div");
+
+        
+        let nameValue = document.createTextNode(element.name);
+        let messageValue = document.createTextNode(element.message);
 
 
+        let h2NameValue = document.createElement("h2");
+        h2NameValue.appendChild(nameValue);
+        let h2MessageValue = document.createElement("h2");
+        h2MessageValue.appendChild(messageValue);
 
-    div.appendChild(nameValue);
-    div.appendChild(messageValue);
+        let del = document.createElement("button");
+        let deletTxt = document.createTextNode("Delete");
+        del.appendChild(deletTxt);
+        del.addEventListener("click", ()=>{
+           let li = del.parentElement.parentElement;
+           deleteMessage(li.id);
+           refreshMessages();
+        })
 
-    li.appendChild(div);
 
-    ul.appendChild(li);
+        div.appendChild(nameValue);
+        div.appendChild(messageValue);
+        div.appendChild(del);
+
+        li.appendChild(div);
+        li.classList.add("messagesInd");
+        ul.appendChild(li);
+    });
+
 }   
