698e53880297b524f60bb23bb7737fb753baf63e
Denilson <denilsonsousa7@outlook.com>
2020-08-24 19:38:23 -0300

    add creditos

---
+++ js/credits.js
@@ -0,0 +1,8 @@
+var credit = document.getElementById("credits");
+
+credit.addEventListener('click',(e)=>{
+    e.preventDefault();
+    alert("Desenvolvimento de software para WEB \n"+
+            "Devs: Denilson Sousa\n"+
+            "          Ismael moreira")
+})
